5b05e43139c1e1c9f3e545c9dd4a2be749561ab1
Gabriel Guedes <gabriel.guedes@projetoomni.com>
2023-10-07 23:36:45 -0300

    feat: add jwt auth guard

---
+++ src/auth-req.ts
@@ -0,0 +1,5 @@
+export interface AuthReq extends Request {
+  user: {
+    cpf: string;
+  }
+} 
---
+++ src/auth/guards/jwt.guard.ts
@@ -0,0 +1,37 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtAuthService } from '../../jwt-auth/jwt-auth.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private jwtAuthService: JwtAuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtAuthService.validateToken(token);
+      request['user'] = {
+        cpf: payload.sub,
+      }
+    } catch {
+      throw new UnauthorizedException();
+    }
+    
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
